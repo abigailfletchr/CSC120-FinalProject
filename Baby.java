63f9c218fd1646b0094dcf6f1e8bc11196076ba5
Your Name <you@example.com>
2023-04-27 11:30:55 -0400

    baby constructor

--- Baby.java
+++ Baby.java
@@ -12,9 +12,17 @@ public class Baby {
 
 
     // constructor/s go here
-
-
-
+    public Baby(double weight, String name, String hair, String stuffedAnimal, String stage, int motorSkills,
+            int trustConnect, int speechSkills) {
+        this.weight = weight;
+        this.name = name;
+        this.hair = hair;
+        this.stuffedAnimal = stuffedAnimal;
+        this.stage = stage;
+        this.motorSkills = motorSkills;
+        this.trustConnect = trustConnect;
+        this.speechSkills = speechSkills;
+    }
 
     // setters
     public void setStage() {
